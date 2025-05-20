fd1f20868495176efa70069e892b1d86376f94e4
Aritra-Dey-117-XT <ece22127@iiitkalyani.ac.in>
2025-05-20 21:18:24 +0530

    Connected MongoDB Server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB Database!")
+    }).catch((err) => {
+        console.log(err)
+    })
 
 app.listen(3000, () => {
     console.log("Server Listening on Port 3000!")
